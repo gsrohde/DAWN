0f0d04d8772c941bfe4bbd3ab5de22c16535f19f
Scott Rohde <srohde@illinois.edu>
2023-05-31 14:50:37 -0500

    Make sure the schema uri points to the latest version.

--- app/compilation_options.h
+++ app/compilation_options.h
@@ -7,7 +7,7 @@
 
 
 #ifndef SIMULATION_INPUT_SCHEMA_URI
-#define SIMULATION_INPUT_SCHEMA_URI https:\/\/gist.githubusercontent.com/gsrohde/3c1d3be487b94234de9ad1312be27491/raw/a853bd9fff1dcce81a5fec55d870e59df93ec8a4/simulation_input.xsd
+#define SIMULATION_INPUT_SCHEMA_URI https:\/\/gist.githubusercontent.com/gsrohde/3c1d3be487b94234de9ad1312be27491/raw
 #endif
 
 #define SIMULATION_INPUT_SCHEMA_URI_STRING STR(SIMULATION_INPUT_SCHEMA_URI)
