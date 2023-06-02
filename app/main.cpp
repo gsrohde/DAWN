ea5b795151ed026b556ebdf63894f9fd69baf15b
Scott Rohde <srohde@illinois.edu>
2023-06-01 19:40:29 -0500

    Catch all exceptions in main.

--- app/main.cpp
+++ app/main.cpp
@@ -11,26 +11,40 @@ int main(int argC, char* argV[]) {
 
     Option_parser op(argC, argV);
 
-    Simulation_definition sim_def(op.get_spec_file(),
-                                  op.get_parser_options());
+    try {
+        Simulation_definition sim_def(op.get_spec_file(),
+                                      op.get_parser_options());
 
-    biocro_simulation sim {
-        sim_def.get_initial_state(),
-        sim_def.get_parameters(),
-        sim_def.get_drivers(),
-        sim_def.get_direct_modules(),
-        sim_def.get_differential_modules(),
+        biocro_simulation sim {
+            sim_def.get_initial_state(),
+            sim_def.get_parameters(),
+            sim_def.get_drivers(),
+            sim_def.get_direct_modules(),
+            sim_def.get_differential_modules(),
 
-        "homemade_euler",
-        1,
-        0.0001,
-        0.0001,
-        200
-    };
+            "homemade_euler",
+            1,
+            0.0001,
+            0.0001,
+            200
+        };
 
-    auto result = sim.run_simulation();
+        auto result = sim.run_simulation();
 
-    Result_xml_document doc(result);
-    doc.print(op.get_output_file());
+        Result_xml_document doc(result);
+        doc.print(op.get_output_file());
+    }
+    catch(std::runtime_error& e) {
+        cerr << "RUNTIME ERROR: " << e.what() << endl;
+        exit(1);
+    }
+    catch(std::exception& e)   {
+        cerr << "EXCEPTION: " << e.what() << endl;
+        exit(1);
+    }
+    catch(...) {
+        cerr << "UNKNOWN EXCEPTION" << endl;
+        exit(1);
+    }
 }
                            
