4de97a6f16de08c631afe931c6b8d40394dab626
Scott Rohde <srohde@illinois.edu>
2023-06-02 11:55:13 -0500

    Re-indented.

--- app/Result_xml_document.cpp
+++ app/Result_xml_document.cpp
@@ -2,66 +2,64 @@
 #include "Result_xml_document.h"
 
 Result_xml_document::Result_xml_document(const state_vector_map& result) {
-   try {
-      XMLPlatformUtils::Initialize();  // Initialize Xerces infrastructure
-   } catch( XMLException& e ) {
-      char* message = XMLString::transcode( e.getMessage() );
-      cerr << "XML toolkit initialization error: " << message << endl;
-      XMLString::release( &message );
-      // throw exception here to return ERROR_XERCES_INIT
-   }
+    try {
+        XMLPlatformUtils::Initialize();  // Initialize Xerces infrastructure
+    } catch( XMLException& e ) {
+        char* message = XMLString::transcode( e.getMessage() );
+        cerr << "XML toolkit initialization error: " << message << endl;
+        XMLString::release( &message );
+        // throw exception here to return ERROR_XERCES_INIT
+    }
 
-   DOMImplementation* impl =  DOMImplementationRegistry::getDOMImplementation(X("Core"));
+    DOMImplementation* impl =  DOMImplementationRegistry::getDOMImplementation(X("Core"));
 
-   if (impl != NULL) {
-       try {
-           doc = impl->createDocument(
-                           nullptr,                    // root element namespace URI.
-                           X("simulation-result"),     // root element name
-                           nullptr);                   // document type object (DTD).
+    if (impl != NULL) {
+        try {
+            doc = impl->createDocument(
+                                       nullptr,                    // root element namespace URI.
+                                       X("simulation-result"),     // root element name
+                                       nullptr);                   // document type object (DTD).
            
-           auto some_column = result.begin();
+            auto some_column = result.begin();
 
-           auto number_of_rows = some_column->second.size();
+            auto number_of_rows = some_column->second.size();
 
-           for (int i = 0; i < number_of_rows; ++i) {
-               add_row(result, i);
-           }
+            for (int i = 0; i < number_of_rows; ++i) {
+                add_row(result, i);
+            }
            
-           }
-           catch (const OutOfMemoryException&)
-           {
-               cerr << "OutOfMemoryException" << endl;
-               errorCode = 5;
-           }
-           catch (const DOMException& e)
-           {
-               cerr << "DOMException code is:  " << e.code << endl;
-               errorCode = 2;
-           }
-           catch (...)
-           {
-               cerr << "An error occurred creating the document" << endl;
-               errorCode = 3;
-           }
-       }  // (inpl != NULL)
-   else {
-       cerr << "Requested implementation is not supported" << endl;
-       errorCode = 4;
-   }
+        }
+        catch (const OutOfMemoryException&) {
+            cerr << "OutOfMemoryException" << endl;
+            errorCode = 5;
+        }
+        catch (const DOMException& e) {
+            cerr << "DOMException code is:  " << e.code << endl;
+            errorCode = 2;
+        }
+        catch (...) {
+            cerr << "An error occurred creating the document" << endl;
+            errorCode = 3;
+        }
+    }  // (inpl != NULL)
+    else {
+        cerr << "Requested implementation is not supported" << endl;
+        errorCode = 4;
+    }
 }
 
 Result_xml_document::~Result_xml_document() {
-   try {
-       doc->release();
+    try {
+        doc->release();
        
-       XMLPlatformUtils::Terminate();  // Terminate after release of memory
-   } catch( xercesc::XMLException& e ) {
-      char* message = xercesc::XMLString::transcode( e.getMessage() );
+        XMLPlatformUtils::Terminate();  // Terminate after release of memory
+    }
+    catch( xercesc::XMLException& e ) {
+        char* message = xercesc::XMLString::transcode( e.getMessage() );
 
-      cerr << "XML ttolkit teardown error: " << message << endl;
-      XMLString::release( &message );
-   }
+        cerr << "XML ttolkit teardown error: " << message << endl;
+        XMLString::release( &message );
+    }
 }
 
 void Result_xml_document::print() {
@@ -80,7 +78,8 @@ void Result_xml_document::print(string output_file) {
     // choose between file and stdout
     if (output_file.empty()) {
         myFormTarget=new StdOutFormatTarget();
-    } else {
+    }
+    else {
         myFormTarget=new LocalFileFormatTarget(output_file.c_str());
     }
 
