d32d5587e813cd8ffcfe91a5d0e5cfb54048a590
Scott Rohde <srohde@illinois.edu>
2023-06-01 20:51:12 -0500

    Added command-line option to show schema URI.

---
+++ app/option_parser.cpp
@@ -0,0 +1,60 @@
+#include "option_parser.h"
+#include "compilation_options.h"
+
+using std::cerr;
+using std::endl;
+using std::string;
+
+
+Option_parser::Option_parser(int argC, char* argV[]) {
+
+    // Check command line and extract arguments.
+    if (argC < 2) {
+        usage();
+        exit(1);
+    }
+
+
+    // See if non validating dom parser configuration is requested.
+    int parmInd;
+    for (parmInd = 1; parmInd < argC; parmInd++) {
+        // Break out on first parm not starting with a dash
+        if (argV[parmInd][0] != '-') {
+            break;
+        }
+
+        // Watch for special case help request
+        if (!strcmp(argV[parmInd], "-h")) {
+            usage();
+            exit(0);
+        }
+        else if (!strncmp(argV[parmInd], "-show_default_schema_uri", 24)) {
+            cout << "Default schema URI: " << SIMULATION_INPUT_SCHEMA_URI_STRING << endl;
+            exit(0);
+        }
+        else if (!strncmp(argV[parmInd], "-v=", 3)) {
+            const char* const parm = &argV[parmInd][3];
+
+            if (!strcmp(parm, "never") || !strcmp(parm, "auto") || !strcmp(parm, "always")) {
+                parser_options["validation_scheme"] = string(parm);
+            }
+            else {
+                cerr << "Unknown -v= value: " << parm << endl;
+                exit(1);
+            }
+        }
+        else if (!strncmp(argV[parmInd], "-wfile=", 7)) {
+            output_file =  &(argV[parmInd][7]);
+        }
+        else {
+            cerr << "Unknown option '" << argV[parmInd]
+                 << "', ignoring it.\n" << endl;
+        }
+    }
+
+    if (parmInd + 1 != argC) {
+        usage();
+        exit(1);
+    }
+    specification_file = argV[parmInd];
+}
--- app/option_parser.h
+++ app/option_parser.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 
 using std::cerr;
 using std::cout;
@@ -12,54 +13,7 @@ using Flag_map = std::unordered_map<string, bool>;
 
 class Option_parser {
  public:
-    Option_parser(int argC, char* argV[]) {
-
-        // Check command line and extract arguments.
-        if (argC < 2) {
-            usage();
-            exit(1);
-        }
-
-
-        // See if non validating dom parser configuration is requested.
-        int parmInd;
-        for (parmInd = 1; parmInd < argC; parmInd++) {
-            // Break out on first parm not starting with a dash
-            if (argV[parmInd][0] != '-') {
-                break;
-            }
-
-            // Watch for special case help request
-            if (!strcmp(argV[parmInd], "-h")) {
-                usage();
-                exit(0);
-            }
-            else if (!strncmp(argV[parmInd], "-v=", 3)) {
-                const char* const parm = &argV[parmInd][3];
-
-                if (!strcmp(parm, "never") || !strcmp(parm, "auto") || !strcmp(parm, "always")) {
-                    parser_options["validation_scheme"] = string(parm);
-                }
-                else {
-                    cerr << "Unknown -v= value: " << parm << endl;
-                    exit(1);
-                }
-            }
-            else if (!strncmp(argV[parmInd], "-wfile=", 7)) {
-                output_file =  &(argV[parmInd][7]);
-            }
-            else {
-                cerr << "Unknown option '" << argV[parmInd]
-                     << "', ignoring it.\n" << endl;
-            }
-        }
-
-        if (parmInd + 1 != argC) {
-            usage();
-            exit(1);
-        }
-        specification_file = argV[parmInd];
-    }
+    Option_parser(int argC, char* argV[]);
 
     Option_map get_parser_options() {
         return parser_options;
@@ -76,13 +30,16 @@ class Option_parser {
     void usage()
     {
         cout << "\nUsage:\n"
-            "    main [options] <XML file>\n\n"
+            "    main [options] <XML file>\n"
+            "    main -h\n"
+            "    main -show_default_schema_uri\n\n"
             "This program reads the dynamical system specification <XML file>, \n"
-            "runs the simulation, and writes the result to standard output.\n"
+            "runs the simulation, and writes the result to standard output.\n\n"
             "Options:\n"
-            "    -v=xxx      Validation scheme [always* | never | auto].\n"
-            "    -wfile=xxx  Write to a file instead of stdout.\n"
-            "    -h          Show this help.\n\n"
+            "    -v=xxx                    Validation scheme [always* | never | auto].\n"
+            "    -wfile=xxx                Write to a file instead of stdout.\n"
+            "    -h                        Show this help.\n"
+            "    -show_default_schema_uri  Show the default location of the schema file used in validating input.\n\n"
             "  * = Default if not provided explicitly.\n\n"
             "The parser has intrinsic support for the following encodings:\n"
             "    UTF-8, US-ASCII, ISO8859-1, UTF-16[BL]E, UCS-4[BL]E,\n"
