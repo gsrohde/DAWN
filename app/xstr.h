1d115fab7f6905c0bdced04c02436e926b08326b
Scott Rohde <srohde@illinois.edu>
2023-06-07 14:35:50 -0500

    Forgot to add file xstr.h when factored out of Result_xml_document.cpp in commit 34c4755.  Adding it now.

---
+++ app/xstr.h
@@ -0,0 +1,48 @@
+#ifndef XSTR_H
+#define XSTR_H
+
+/* Xerces Library */
+#include <xercesc/util/XMLString.hpp>
+
+using xercesc::XMLString;
+
+class XStr
+{
+public :
+    // -----------------------------------------------------------------------
+    //  Constructors and Destructor
+    // -----------------------------------------------------------------------
+    XStr(const char* const toTranscode)
+    {
+        // Call the private transcoding method
+        fUnicodeForm = XMLString::transcode(toTranscode);
+    }
+
+    ~XStr()
+    {
+        XMLString::release(&fUnicodeForm);
+    }
+
+
+    // -----------------------------------------------------------------------
+    //  Getter methods
+    // -----------------------------------------------------------------------
+    const XMLCh* unicodeForm() const
+    {
+        return fUnicodeForm;
+    }
+
+private :
+    // -----------------------------------------------------------------------
+    //  Private data members
+    //
+    //  fUnicodeForm
+    //      This is the Unicode XMLCh format of the string.
+    // -----------------------------------------------------------------------
+    XMLCh* fUnicodeForm;
+};
+
+#define X(str) XStr(str).unicodeForm()
+
+
+#endif
