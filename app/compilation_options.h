24ec489f3d8362a54a5c7bfedf3c6fb73bf8c666
Scott Rohde <srohde@illinois.edu>
2023-09-01 17:42:50 -0500

    Simplified get_schema_uri, eliminating compilation warning: "unknown escape sequence '\/'".

--- app/compilation_options.h
+++ app/compilation_options.h
@@ -13,8 +13,9 @@ inline string get_schema_uri() {
 #if defined(USE_LOCAL_SCHEMA_FILE) && (USE_LOCAL_SCHEMA_FILE == true) && defined(LOCAL_SCHEMA_FILEPATH)
     return STR(LOCAL_SCHEMA_FILEPATH);
 #else
-    #define SIMULATION_INPUT_SCHEMA_URI https:\/\/raw.githubusercontent.com/gsrohde/DAWN/revise_schema_setting_mechanism/app/xml_schema/simulation_input.xsd
-    return STR(SIMULATION_INPUT_SCHEMA_URI);
+    return "https://raw.githubusercontent.com"
+        "/gsrohde/DAWN/revise_schema_setting_mechanism/"
+        "app/xml_schema/simulation_input.xsd";
 #endif
 }
 
