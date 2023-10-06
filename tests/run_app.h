bfd521d0b42dfa1a0e25bdb5ba69ab77a6e45f80
Scott Rohde <srohde@illinois.edu>
2023-10-06 16:35:06 -0500

    Added a regression test.

---
+++ tests/run_app.h
@@ -0,0 +1,51 @@
+/* BioCro Library */
+#include <framework/biocro_simulation.h>
+#include <framework/module_factory.h>
+#include <module_library/module_library.h>
+
+/* DAWN app */
+#include "option_parser.h"
+#include "simulation_definition.h"
+#include "Result_xml_document.h"
+
+inline Result_xml_document run_app(int argC, char** argV) {
+
+    Option_parser op(argC, argV);
+
+    try {
+        Simulation_definition sim_def(op);
+
+        auto solver = sim_def.solver();
+
+        biocro_simulation sim {
+            sim_def.get_initial_state(),
+            sim_def.get_parameters(),
+            sim_def.get_drivers(),
+            sim_def.get_direct_modules(),
+            sim_def.get_differential_modules(),
+
+            solver.name,
+            solver.step_size,
+            solver.relative_tolerance,
+            solver.absolute_tolerance,
+            solver.max_steps
+        };
+
+        auto result = sim.run_simulation();
+
+        Result_xml_document doc(result);
+        return doc;
+    }
+    catch(std::runtime_error& e) {
+        cerr << "RUNTIME ERROR: " << e.what() << endl;
+        exit(1);
+    }
+    catch(std::exception& e)   {
+        cerr << "EXCEPTION: " << e.what() << endl;
+        exit(1);
+    }
+    catch(...) {
+        cerr << "UNKNOWN EXCEPTION" << endl;
+        exit(1);
+    }
+}
---
+++ tests/test_regression.cpp
@@ -0,0 +1,47 @@
+#include <gtest/gtest.h>
+
+#include <string>
+
+#include <StrX.h>
+#include <xstr.h> // includes XX() macro
+
+#include "run_app.h"
+
+using namespace std;
+using namespace xercesc;
+
+
+
+TEST(RegressionTests, TestCommandlineParsing) {
+    const int argC {2};
+    char* argV[argC + 1] {
+         const_cast<char*>(""),
+         const_cast<char*>("../app/sample_input/harmonic_oscillator_system.xml"),
+         nullptr };
+
+    auto result = run_app(2, argV);
+    DOMNodeList* row_list = result.get_elements_by_tag_name("row");
+    DOMElement* last_item = dynamic_cast< DOMElement* >(row_list->item(row_list->getLength() - 1));
+    DOMNodeList* variable_list = last_item->getElementsByTagName(XX("variable"));
+
+    // The EXPECT test below makes use of the fact that
+    // getElementsByTagName returns matching elements in document
+    // order, which happens to be alphabetical order.
+    double expected_value[] = {
+        6.28, // elapsed_time
+        0.000507307, // kinetic_energy
+        9.99995, // position
+        49.9995, // spring_energy
+        50, // total_energy
+        0.031853 // velocity
+    };
+            
+    for (int i = 0; i < variable_list->getLength(); ++i) {
+        DOMElement* variable = dynamic_cast< DOMElement* >(variable_list->item(i));
+        const XMLCh* name = variable->getAttribute(XX("name"));
+        const XMLCh* value = variable->getAttribute(XX("value"));
+        // cout << StrX(name) << ": " << StrX(value) << endl;
+
+        EXPECT_DOUBLE_EQ(std::stod(string(StrX(value).localForm())), expected_value[i]);
+    }
+}
