0a422efe83c07c5e75349357dc9a03b7a9e82f89
Scott Rohde <srohde@illinois.edu>
2023-10-06 20:22:34 -0500

    Factored out some potentially repetitive code from regression test.

--- tests/test_regression.cpp
+++ tests/test_regression.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <string>
+#include <vector>
 
 #include <StrX.h>
 #include <xstr.h> // includes XX() macro
@@ -11,31 +12,11 @@ using namespace std;
 using namespace xercesc;
 
 
-
-TEST(RegressionTests, TestCommandlineParsing) {
-    const int argC {2};
-    char* argV[argC + 1] {
-         const_cast<char*>(""),
-         const_cast<char*>("../app/sample_input/harmonic_oscillator_system.xml"),
-         nullptr };
-
-    auto result = run_app(2, argV);
+void compare_last_row_with_expected(Result_xml_document& result, const vector<double>& expected_value) {
     DOMNodeList* row_list = result.get_elements_by_tag_name("row");
     DOMElement* last_item = dynamic_cast< DOMElement* >(row_list->item(row_list->getLength() - 1));
     DOMNodeList* variable_list = last_item->getElementsByTagName(XX("variable"));
 
-    // The EXPECT test below makes use of the fact that
-    // getElementsByTagName returns matching elements in document
-    // order, which happens to be alphabetical order.
-    double expected_value[] = {
-        6.28, // elapsed_time
-        0.000507307, // kinetic_energy
-        9.99995, // position
-        49.9995, // spring_energy
-        50, // total_energy
-        0.031853 // velocity
-    };
-            
     for (int i = 0; i < variable_list->getLength(); ++i) {
         DOMElement* variable = dynamic_cast< DOMElement* >(variable_list->item(i));
         const XMLCh* name = variable->getAttribute(XX("name"));
@@ -44,4 +25,29 @@ TEST(RegressionTests, TestCommandlineParsing) {
 
         EXPECT_DOUBLE_EQ(std::stod(string(StrX(value).localForm())), expected_value[i]);
     }
+}
+
+
+TEST(RegressionTests, TestHarmonicOscillator) {
+    const int argC {2};
+    char* argV[argC + 1] {
+         const_cast<char*>(""),
+         const_cast<char*>("../app/sample_input/harmonic_oscillator_system.xml"),
+         nullptr };
+
+    auto result = run_app(2, argV);
+
+    // The EXPECT test below makes use of the fact that
+    // getElementsByTagName returns matching elements in document
+    // order, which happens to be alphabetical order.
+    vector<double> expected_value {
+        6.28, // elapsed_time
+        0.000507307, // kinetic_energy
+        9.99995, // position
+        49.9995, // spring_energy
+        50, // total_energy
+        0.031853 // velocity
+    };
+
+    compare_last_row_with_expected(result, expected_value);
 }
