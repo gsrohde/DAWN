30ba51e0635ef1c6c31dd9bc7f7eafbb789f7705
Scott Rohde <srohde@illinois.edu>
2024-01-25 15:08:21 -0600

    Disable (for now at least) use of doxygen-awesome-css stylesheet.

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -1,13 +1,13 @@
 find_package(Doxygen REQUIRED)
 
-include(FetchContent)
-FetchContent_Declare(doxygen-awesome-css
-  GIT_REPOSITORY
-    https://github.com/jothepro/doxygen-awesome-css.git
-  GIT_TAG
-    v1.6.0
-)
-FetchContent_MakeAvailable(doxygen-awesome-css)
+# include(FetchContent)
+# FetchContent_Declare(doxygen-awesome-css
+#   GIT_REPOSITORY
+#     https://github.com/jothepro/doxygen-awesome-css.git
+#   GIT_TAG
+#     v1.6.0
+# )
+# FetchContent_MakeAvailable(doxygen-awesome-css)
 
 function(Doxygen target input)
   set(NAME "doxygen-${target}")
@@ -17,8 +17,8 @@ function(Doxygen target input)
   set(DOXYGEN_HAVE_DOT              YES)
   set(DOXYGEN_DOT_IMAGE_FORMAT      svg)
   set(DOXYGEN_DOT_TRANSPARENT       YES)
-  set(DOXYGEN_HTML_EXTRA_STYLESHEET
-      ${doxygen-awesome-css_SOURCE_DIR}/doxygen-awesome.css)
+# set(DOXYGEN_HTML_EXTRA_STYLESHEET
+#     ${doxygen-awesome-css_SOURCE_DIR}/doxygen-awesome.css)
 
   doxygen_add_docs(${NAME}
       ${PROJECT_SOURCE_DIR}/${input}
