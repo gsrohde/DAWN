3c9fc02eaf72e616f5e9e68152ee849f28f9dead
Scott Rohde <srohde@illinois.edu>
2024-02-13 16:49:47 -0600

    Updated version of biocro-lib used and updated sample input files and regression tests to be consistent with changes to BioCro.

--- tests/test_regression.cpp
+++ tests/test_regression.cpp
@@ -67,25 +67,26 @@ TEST(RegressionTests, TestMiscanthus2002) {
     vector<double> expected_value {
         0, // Grain
         0, // GrossAssim
-        2.06262, // Leaf
-        0.292759, // LeafLitter
-        0.389492, // LeafWS
-        11.1541, // Rhizome
+        2.49952, // Leaf
+        0.421836, // LeafLitter
+        0.37359, // LeafWS
+        18.8271, // Rhizome
         0, // RhizomeLitter
-        2.87031, // Root
+        3.74252, // Root
         0, // RootLitter
         0, // Shell
         1.7, // Sp
-        30.0035, // Stem
-        0.639085, // StemLitter
-        0.760048, // StomataWS
+        51.4725, // Stem
+        0.822043, // StemLitter
+        0.749953, // StomataWS
         3867.71, // TTc
         0.04, // alpha
-        -0.000775326, // canopy_assimilation_rate
-        214.048, // canopy_conductance
-        0.250165, // canopy_transpiration_rate
+        -0.000933939, // canopy_assimilation_rate
+        255.997, // canopy_conductance
+        0, // canopy_photorespiration_rate
+        0.493819, // canopy_transpiration_rate
         -0.84183, // cosine_zenith_angle
-        0.204823, // cws1
+        0.199977, // cws1
         0.32, // cws2
         287, // doy
         6.57081, // gmst
@@ -96,7 +97,7 @@ TEST(RegressionTests, TestMiscanthus2002) {
         0.35, // kRhizome
         0.01, // kRoot
         0.63, // kStem
-        3.50646, // lai
+        4.24918, // lai
         1088, // leaf_senescence_index
         170.531, // lha
         0.704144, // lmst
@@ -109,8 +110,8 @@ TEST(RegressionTests, TestMiscanthus2002) {
         0.62275, // rh
         1869, // rhizome_senescence_index
         0, // root_senescence_index
-        0.697322, // soil_evaporation_rate
-        0.291206, // soil_water_content
+        0.420345, // soil_evaporation_rate
+        0.289994, // soil_water_content
         0, // solar
         203.562, // solar_L
         342.452, // solar_azimuth_angle
@@ -143,26 +144,27 @@ TEST(RegressionTests, TestMiscanthus2005) {
     // order, which happens to be alphabetical order.
     vector<double> expected_value {
         0, // Grain
-        9.83485e-06, // GrossAssim
-        1.67089, // Leaf
-        0.324997, // LeafLitter
-        0.389335, // LeafWS
-        10.9034, // Rhizome
+        1.12571e-05, // GrossAssim
+        1.95722, // Leaf
+        0.436394, // LeafLitter
+        0.383818, // LeafWS
+        17.3966, // Rhizome
         0, // RhizomeLitter
-        2.66448, // Root
+        3.31794, // Root
         0, // RootLitter
         0, // Shell
         1.7, // Sp
-        27.9671, // Stem
-        0.522391, // StemLitter
-        0.759949, // StomataWS
+        45.8852, // Stem
+        0.631005, // StemLitter
+        0.756489, // StomataWS
         3681.38, // TTc
         0.04, // alpha
-        -0.000604756, // canopy_assimilation_rate
-        173.373, // canopy_conductance
-        0.118093, // canopy_transpiration_rate
+        -0.000699726, // canopy_assimilation_rate
+        202.174, // canopy_conductance
+        0, // canopy_photorespiration_rate
+        0.243641, // canopy_transpiration_rate
         -0.885732, // cosine_zenith_angle
-        0.204776, // cws1
+        0.203114, // cws1
         0.32, // cws2
         301, // doy
         7.50872, // gmst
@@ -173,7 +175,7 @@ TEST(RegressionTests, TestMiscanthus2005) {
         0.35, // kRhizome
         0.01, // kRoot
         0.63, // kStem
-        2.84051, // lai
+        3.32728, // lai
         1060, // leaf_senescence_index
         171.066, // lha
         1.64205, // lmst
@@ -186,8 +188,8 @@ TEST(RegressionTests, TestMiscanthus2005) {
         0.75365, // rh
         1525, // rhizome_senescence_index
         0, // root_senescence_index
-        1.08769, // soil_evaporation_rate
-        0.291194, // soil_water_content
+        0.780886, // soil_evaporation_rate
+        0.290779, // soil_water_content
         0.092, // solar
         217.63, // solar_L
         341.014, // solar_azimuth_angle
@@ -219,24 +221,26 @@ TEST(RegressionTests, TestWillow2005) {
     // getElementsByTagName returns matching elements in document
     // order, which happens to be alphabetical order.
     vector<double> expected_value {
-        0, // Grain
-        5.05994e-07, // GrossAssim
-        0.00578966, // Leaf
-        0.404188, // LeafLitter
+        0, //         Grain
+        1.97318e-08, // GrossAssim
+        0.00661579, // Leaf
+        0.461861, // LeafLitter
         0.821445, // LeafWS
-        1.46608, // Rhizome
+        1.61669, // Rhizome
         0, // RhizomeLitter
-        1.5961, // Root
+        1.71325, // Root
         0, // RootLitter
         0, // Shell
         1.1, // Sp
-        5.14999, // Stem
+        6.02343, // Stem
         0, // StemLitter
-        0.75023, // StomataWS
+        0.750229, // StomataWS
         3681.38, // TTc
         0, // alpha
-        -1.01644e-06, // canopy_assimilation_rate
-        0.000224362, // canopy_transpiration_rate
+        -1.50824e-06, // canopy_assimilation_rate
+        0.438593, // canopy_conductance
+        6.66891e-10, // canopy_photorespiration_rate
+        0.000393817, // canopy_transpiration_rate
         -0.885732, // cosine_zenith_angle
         0.20011, // cws1
         0.32, // cws2
@@ -249,7 +253,7 @@ TEST(RegressionTests, TestWillow2005) {
         0.15, // kRhizome
         0.15, // kRoot
         0.7, // kStem
-        0.00636863, // lai
+        0.00727737, // lai
         5, // leafdeathrate
         171.066, // lha
         1.64205, // lmst
@@ -262,7 +266,7 @@ TEST(RegressionTests, TestWillow2005) {
         0.75365, // rh
         369, // rhizome_senescence_index
         0, // root_senescence_index
-        7.46476, // soil_evaporation_rate
+        7.46015, // soil_evaporation_rate
         0.290028, // soil_water_content
         0.092, // solar
         217.63, // solar_L
@@ -295,24 +299,26 @@ TEST(RegressionTests, TestWillow2002) {
     // getElementsByTagName returns matching elements in document
     // order, which happens to be alphabetical order.
     vector<double> expected_value {
-        0, // Grain
-        5.26291e-07, // GrossAssim
-        0.00615591, // Leaf
-        0.427956, // LeafLitter
-        0.821547, // LeafWS
-        1.46428, // Rhizome
+        0, //         Grain
+        0, // GrossAssim
+        0.00747925, // Leaf
+        0.519952, // LeafLitter
+        0.821546, // LeafWS
+        1.67643, // Rhizome
         0, // RhizomeLitter
-        1.60745, // Root
+        1.76373, // Root
         0, // RootLitter
         0, // Shell
         1.1, // Sp
-        5.3346, // Stem
+        6.58827, // Stem
         0, // StemLitter
-        0.750379, // StomataWS
+        0.750378, // StomataWS
         3867.71, // TTc
         0, // alpha
-        -1.10745e-06, // canopy_assimilation_rate
-        0.000408679, // canopy_transpiration_rate
+        -1.7237e-06, // canopy_assimilation_rate
+        0.495933, // canopy_conductance
+        0, // canopy_photorespiration_rate
+        0.000698578, // canopy_transpiration_rate
         -0.84183, // cosine_zenith_angle
         0.200182, // cws1
         0.32, // cws2
@@ -325,7 +331,7 @@ TEST(RegressionTests, TestWillow2002) {
         0.15, // kRhizome
         0.15, // kRoot
         0.7, // kStem
-        0.0067715, // lai
+        0.00822717, // lai
         5, // leafdeathrate
         170.531, // lha
         0.704144, // lmst
@@ -338,8 +344,8 @@ TEST(RegressionTests, TestWillow2002) {
         0.62275, // rh
         420, // rhizome_senescence_index
         0, // root_senescence_index
-        7.52482, // soil_evaporation_rate
-        0.290046, // soil_water_content
+        7.51738, // soil_evaporation_rate
+        0.290045, // soil_water_content
         0, // solar
         203.562, // solar_L
         342.452, // solar_azimuth_angle
