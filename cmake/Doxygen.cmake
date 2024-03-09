4d00bd2c0f91c07d9db511b05115254142bd51fe
Scott Rohde <srohde@illinois.edu>
2024-03-08 21:08:47 -0600

    Add CMake variables for customizing Doxygen output.

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -9,14 +9,16 @@ else()
     "available in order to build the Doxygen documentation.")
 endif()
 
-# include(FetchContent)
-# FetchContent_Declare(doxygen-awesome-css
-#   GIT_REPOSITORY
-#     https://github.com/jothepro/doxygen-awesome-css.git
-#   GIT_TAG
-#     v1.6.0
-# )
-# FetchContent_MakeAvailable(doxygen-awesome-css)
+if(USE_DOXYGEN_AWESOME_STYLESHEET)
+  include(FetchContent)
+  FetchContent_Declare(doxygen-awesome-css
+    GIT_REPOSITORY
+      https://github.com/jothepro/doxygen-awesome-css.git
+    GIT_TAG
+      v1.6.0
+  )
+  FetchContent_MakeAvailable(doxygen-awesome-css)
+endif()
 
 function(Doxygen target)
   set(input ${ARGN})
@@ -25,15 +27,26 @@ function(Doxygen target)
   set(NAME "${target}")
   set(DOXYGEN_HTML_OUTPUT     ${PROJECT_BINARY_DIR}/${NAME})
   set(DOXYGEN_GENERATE_HTML         YES)
-  set(DOXYGEN_GENERATE_TREEVIEW     YES)
+  if(ADD_TREEVIEW)
+    set(DOXYGEN_GENERATE_TREEVIEW   YES)
+  else()
+    set(DOXYGEN_GENERATE_TREEVIEW    NO)
+  endif()
   set(DOXYGEN_HAVE_DOT              YES)
   set(DOXYGEN_DOT_IMAGE_FORMAT      svg)
   set(DOXYGEN_DOT_TRANSPARENT       YES)
   set(DOXYGEN_EXTRACT_ALL           YES)
   set(DOXYGEN_SOURCE_BROWSER        YES)
+  if(USE_DOXYGEN_AWESOME_STYLESHEET)
+    set(DOXYGEN_HTML_EXTRA_STYLESHEET
+        ${doxygen-awesome-css_SOURCE_DIR}/doxygen-awesome.css)
+  endif()
 
-  # set(DOXYGEN_HTML_EXTRA_STYLESHEET
-  #     ${doxygen-awesome-css_SOURCE_DIR}/doxygen-awesome.css)
+  if(DOCUMENT_PRIVATE_MEMBERS)
+    set(DOXYGEN_EXTRACT_PRIVATE     YES)
+  else()
+    set(DOXYGEN_EXTRACT_PRIVATE      NO)
+  endif()
 
   if(DOXYGEN_FOUND)
     doxygen_add_docs(${NAME}
